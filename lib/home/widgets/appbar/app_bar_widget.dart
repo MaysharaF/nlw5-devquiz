c99afc35ccdee241049a168e336082ff6a8c5f2b
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-04-21 19:27:58 -0300

    Adicionando score card na bar

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -8,27 +9,38 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(gradient: AppGradients.linear),
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Stack(
               children: [
-                Text.rich(TextSpan(
-                    text: "Olá, ",
-                    style: AppTextStyles.title,
-                    children: [
-                      TextSpan(text: "Mayshara", style: AppTextStyles.titleBold)
-                    ])),
                 Container(
-                  width: 58,
-                  height: 58,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            "https://avatars.githubusercontent.com/u/43793682?v=4")),
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: "Mayshara",
+                                style: AppTextStyles.titleBold)
+                          ])),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/43793682?v=4")),
+                        ),
+                      )
+                    ],
                   ),
-                )
+                ),
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
               ],
             ),
           ),
